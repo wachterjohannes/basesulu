7d463d9e37a3479e6390a1391fef4620d054a2e9
Johannes Wachter <johannes@sulu.io>
2019-11-22 07:28:06 +0100

    revert changes for tests and webspace

---
+++ tests/_support/Page/Base.php
@@ -0,0 +1,12 @@
+<?php
+namespace NCA\Tests\Page;
+
+class Base
+{
+    // include url of current page
+    public static $URL = '/';
+
+    public static $navLangEn = '#navLanguage > li:nth-child(2) > a';
+
+    public static $startpageHeadline = 'h1';
+}
